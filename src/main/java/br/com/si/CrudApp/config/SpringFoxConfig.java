ad476e34880ccdaf02ded89d80a69aa2b6ffd231
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-09-13 21:39:13 -0300

    implementa swagger

---
+++ src/main/java/br/com/si/CrudApp/config/SpringFoxConfig.java
@@ -0,0 +1,27 @@
+package br.com.si.CrudApp.config;
+
+import io.swagger.models.auth.In;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+        ;
+    }
+
+
+}
--- src/main/java/br/com/si/CrudApp/service/PersonServiceV2.java
+++ src/main/java/br/com/si/CrudApp/service/PersonServiceV2.java
@@ -14,40 +14,40 @@ public class PersonServiceV2 {
     @Autowired
     private PersonRepositoryV2 repository;
 
-    public Optional<PersonModelV2> findById(long id){
+    public Optional<PersonModelV2> findById(long id) {
         return repository.findById(id);
     }
 
-    public List<PersonModelV2> findAll(){
+    public List<PersonModelV2> findAll() {
         return repository.findAll();
     }
 
-    public PersonModelV2 save(PersonModelV2 model){
+    public PersonModelV2 save(PersonModelV2 model) {
         return repository.save(model);
     }
 
-    public PersonModelV2 update(PersonModelV2 model){
+    public PersonModelV2 update(PersonModelV2 model) {
         Optional<PersonModelV2> p = repository.findById(model.getId());
-        if(p.isPresent()){
+        if (p.isPresent()) {
             p.get().setFullName(model.getFullName());
             p.get().setEmail(model.getEmail());
             p.get().setGender(model.getGender());
             p.get().setBirthDate(model.getBirthDate());
             p.get().setCity(model.getCity());
             return repository.save(p.get());
-        } else {
-            return null;
         }
+        return null;
+
     }
 
-    public void delete(long id){
+    public void delete(long id) {
         Optional<PersonModelV2> p = repository.findById(id);
-        if(p.isPresent()){
+        if (p.isPresent()) {
             repository.delete(p.get());
         }
     }
 
-    public List<PersonModelV2> findByName(String name){
+    public List<PersonModelV2> findByName(String name) {
         return repository.findByFullNameContainsIgnoreCaseOrderByFullName(name);
     }
 
